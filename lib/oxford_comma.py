f8fd1c84bc411515041bfd5e2918959140c96013
Matthew Clark <afromatt6288@hotmail.com>
2023-03-13 15:59:15 -0600

    Lab Completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return "".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        last_item = items.pop()
+        items_list = ", ".join(items)
+        items_with_oxford = f'{items_list}, and {last_item}'
+        return items_with_oxford
